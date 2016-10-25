d90f63012fd4a13e83796efd2446cd1dbcd4948c
ana romero gomez <aromerogomez93@gmail.com>
2016-10-25 10:03:44 +0200

    Proyecto_final

--- juego.py
+++ juego.py
@@ -14,8 +14,9 @@ class Bola(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
         self.rect.centery = HEIGHT / 2
-        self.speed = [0.8, -0.8]
+        self.speed = [0.7, -0.7]
  
+#Actualizamos lo que contiene persona1 que engancha con bola la clase Persona ademas de decirle las posiciones tanto derecha como izquierda arriba y abajo
 
     def actualizar(self, time, persona1):
         self.rect.centerx += self.speed[0] * time
@@ -32,12 +33,18 @@ class Bola(pygame.sprite.Sprite):
             self.speed[0] = -self.speed[0]
             self.rect.centerx += self.speed[0] * time
             self.rect.centery += self.speed[0] * time
+            self.rect.centerx += self.speed[1] * time
+            self.rect.centery += self.speed[1] * time
         if pygame.sprite.collide_rect(self, persona1):
             self.speed[0] = -self.speed[0]
             self.rect.centerx += self.speed[0] * time
             self.rect.centery += self.speed[0] * time
+            self.rect.centerx += self.speed[1] * time
+            self.rect.centery += self.speed[1] * time
+#Aqui le metemos el metodo finalizar juego para que cuando toque el muñeco la pelota nos salga que hemos perdido
+#Aqui lo definimos.
             finalizarJuego(self)
- 
+#Clase persona le ponemos dos posiciones x, y y speed para que nos diga a que velocidad queremos que vaya 
 class Persona(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
@@ -75,7 +82,7 @@ def load_image(filename, transparent=False):
 def main():
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("juego")
-    background_image = load_image('imagenes/foto1.jpg')
+    background_image = load_image('imagenes/foto1.jpg') #imagen de fondo del juego
     bola = Bola()
     persona1 = Persona(630,630)
     clock = pygame.time.Clock()
@@ -86,18 +93,22 @@ def main():
         for eventos in pygame.event.get():
             if eventos.type == QUIT:
                 sys.exit(0)
-        bola.actualizar(time, persona1)
+
+        bola.actualizar(time, persona1) 
         persona1.mover(time, keys)
         screen.blit(background_image, (0, 0))
         screen.blit(persona1.image, persona1.rect)
         screen.blit(bola.image, bola.rect)
         pygame.display.flip()
     return 0
+#Metodo para cuando perdamos en el juego nos salga una pantalla de game over
 def finalizarJuego(self):
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("Juego evita la bola")
+#Nombre del juego
+    pygame.display.set_caption("Juego evita la bola") 
+#Icono del juego
     pygame.display.set_icon(pygame.image.load('imagenes/ana.jpg'))
- 
+#Y fondo de pantalla cuando finalice el juego y perdamos  
     background_image = load_image('imagenes/ana3.png')
 
     while True:
@@ -106,6 +117,7 @@ def finalizarJuego(self):
                 sys.exit(0)
         screen.blit(background_image, (0, 0))
         keys = pygame.key.get_pressed()
+#Aqui le decimos una tecla para que vuelva a empezar una nueva partida
         if keys[K_SPACE]:
             pygame.init()
             main()
